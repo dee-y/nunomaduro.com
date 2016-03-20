26f779747e65ae68cc728cd3262fe6786b6de52d
Nuno Maduro <enunomaduro@gmail.com>
2016-03-20 17:39:32 +0100

    Fixes birthdate number of years

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 $(function () {
 	"use strict";
 	
-    var diff = new Date () - new Date (epoch);
+    var diff = new Date () - new Date ("1990/05/09");
     var age = Math.floor (diff/31536000000);
     $('#age').text (age);
 });
