057daeca628e5955a6f7b2f390afee92b078f496
Nuno Maduro <enunomaduro@gmail.com>
2018-04-02 19:25:38 +0200

    Adds talk

--- resources/views/talks/show.blade.php
+++ resources/views/talks/show.blade.php
@@ -1,6 +1,10 @@
 <h2 class="section-heading text-center">{{ $talk->title }}</h2>
 <a href="#">
+    @if (! empty($talk->youtube_src))
     <iframe width="100%" height="315" src="{{ $talk->youtube_src }}?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    @elseif (! empty($talk->twitter_src))
+        {!! $talk->twitter_src !!}
+    @endif
 </a>
 
 <span class="caption text-muted">{{ $talk->place }}</span>
