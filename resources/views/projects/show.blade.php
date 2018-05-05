04ff2d73c9213bd772df27b92da62006346d2b42
Nuno Maduro <enunomaduro@gmail.com>
2018-05-06 01:47:49 +0200

    Fixes packages url

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,6 +3,6 @@
     <h5 class="card-title">{{ $project->name }}</h5>
     <h6 class="card-subtitle mb-2 text-muted">{{ $project->stargazers_count }} ✨</h6>
     <p class="card-text"><small>{{ $project->description }}</small></p>
-    <a class="btn btn-sm" href="{{ $project->home_url }}"><small>Github</small></a>
+    <a class="btn btn-sm" href="{{ $project->html_url }}"><small>Github</small></a>
   </div>
 </div>
