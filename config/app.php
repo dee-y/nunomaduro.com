37b00dc0e60379370ed78e843c000745a0a6d528
Nuno Maduro <enunomaduro@gmail.com>
2018-05-06 21:08:38 +0200

    Adds twitter card image

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class,
         App\Providers\PackagistServiceProvider::class,
+        App\Providers\SeoServiceProvider::class,
     ],
 
     /*
